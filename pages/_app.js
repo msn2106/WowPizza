5cadfab50a540dd3c99c7c8f03faf450a6e3ec6f
msn2106 <msn2106@gmail.com>
2023-01-20 01:00:49 +0530

    Initial Commit

---
+++ pages/_app.js
@@ -0,0 +1,13 @@
+import Layout from '@/components/Layout'
+// add bootstrap css 
+import 'bootstrap/dist/css/bootstrap.css';
+// own css files here
+import '@/styles/globals.css'
+
+export default function App({ Component, pageProps }) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
+}
